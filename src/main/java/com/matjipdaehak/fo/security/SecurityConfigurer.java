cd171b867da7b1df690bea0aa16e9e21e4858d57
rst0070 <52444137+rst0070@users.noreply.github.com>
2022-05-13 19:21:33 +0900

    changed to using h2 in memory database for test

--- src/main/java/com/matjipdaehak/fo/security/SecurityConfigurer.java
+++ src/main/java/com/matjipdaehak/fo/security/SecurityConfigurer.java
@@ -1,11 +1,27 @@
 package com.matjipdaehak.fo.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
+    @Override
+    public void configure(WebSecurity web){
+        //h2 console에 대한 작업은 시큐리티설정에서 제외
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .anyRequest().permitAll();
+    }
 }
