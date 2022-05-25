41230d6a4d01097574a008486fb42493ed1e320d
rst0070 <52444137+rst0070@users.noreply.github.com>
2022-05-25 17:46:36 +0900

    LoginController status code 반영. LoginController Mock testing.

--- src/main/java/com/matjipdaehak/fo/usermanage/login/controller/LoginController.java
+++ src/main/java/com/matjipdaehak/fo/usermanage/login/controller/LoginController.java
@@ -1,12 +1,8 @@
 package com.matjipdaehak.fo.usermanage.login.controller;
-
-import com.matjipdaehak.fo.exception.AuthorizationException;
 import com.matjipdaehak.fo.usermanage.login.service.LoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.*;
@@ -30,21 +26,25 @@ public class LoginController {
      * {"jwt" : "jwt토큰내용"}방식으로 반환한다.
      * @param req
      * @return json 형식
-     * @throws BadCredentialsException - 인증정보오류
-     * @throws InternalAuthenticationServiceException - jwt생성중 오류
      */
     @RequestMapping
-    public Map<String, String> loginAction(HttpServletRequest req) throws BadCredentialsException, InternalAuthenticationServiceException {
+    public Map<String, String> loginAction(HttpServletRequest req, HttpServletResponse res){
 
         final String authorizationHeader = req.getHeader("Authorization");
         //헤더 정보에 오류있을경우 예외 throw
-        if(authorizationHeader == null || !authorizationHeader.startsWith("Basic ")) throw new BadCredentialsException("the authorization header has problems");
+        if(authorizationHeader == null || !authorizationHeader.startsWith("Basic ")){
+            res.setStatus(401);
+            return Map.of("message", "the authorization header has problems");
+        }
 
 
         String[] decoded;
         try{
             decoded = decodeBase64Str(authorizationHeader.substring(6)).split(":");
-        }catch(Exception ex){throw new BadCredentialsException(ex.getMessage());}
+        }catch(Exception ex){
+            res.setStatus(401);
+            return Map.of("message", "the authorization header has problems");
+        }
 
         String username = decoded[0];
         String password = decoded[1];
